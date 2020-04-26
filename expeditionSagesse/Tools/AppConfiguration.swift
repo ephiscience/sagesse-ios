afb9e3149d043619c724d75c3f94954bbd0cd123
Omar <ommahboubi@airfrance.fr>
2020-04-26 18:57:10 +0200

    add appconfiguration class

--- expeditionSagesse/Tools/AppConfiguration.swift
+++ expeditionSagesse/Tools/AppConfiguration.swift
@@ -13,3 +13,19 @@ public enum SegueIdentifier: String {
     case ChoosePlayersNamesSegue = "ChoosePlayersNamesSegue"
     case StartTheGameSegue = "StartTheGameSegue"
 }
+
+struct DifficultyLevel {
+    var name: String
+    var numberOfCards: Int
+}
+
+class AppConfiguration {
+    
+    static func getDifficultyLevels() -> [DifficultyLevel] {
+        
+        let easy  = DifficultyLevel(name: NSLocalizedString("difficulty.level.easy", comment: "easy"), numberOfCards: 12)
+        let middle  = DifficultyLevel(name: NSLocalizedString("difficulty.level.middle", comment: "middle"), numberOfCards: 24)
+        let hard  = DifficultyLevel(name: NSLocalizedString("difficulty.level.hard", comment: "hard"), numberOfCards: 36)
+        return [easy, middle, hard]
+    }
+}
